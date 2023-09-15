aefd621a629bf11c45637c5ab44c773d4826e7d1
tanaha2002 <tanaha200002@gmail.com>
2023-09-15 11:28:57 +0700

    update get id available options

--- registry_account/add_info_db.py
+++ registry_account/add_info_db.py
@@ -26,10 +26,26 @@ try:
     #create a lead data info about contact name, phone, email, company, website, description
     website = 'https://' + config['DATABASE']['db_name'] + 'berp.vn'
     type_db = config['DATABASE']['type_of_db']
+
+#--------------------method 1: read from record id--------------------
+    # field_name = 'x_subsystem_want_receive'
+    # record_name = 'a'  # Replace with the actual name of the record you created
+
+    # # Get the ID of the record with the specified name
+    # record_id = client.model('crm.lead').search([('name', '=', record_name)], limit=1)
+
+    # #print all lead data from this record  
+    # print(client.model('crm.lead').read(record_id, ['name', 'contact_name', 'phone', 'email_from', 'partner_name', 'website', 'description', 'x_contact_via', 'x_subsystem_want_to_receive', 'x_company_size']))
+#--------------------method 2: read all available options of fields--------------------
+# go to the config (Cấu hình) in CRM -> lead -> subsystem (Phân hệ) -> click on the name of options -> view metadata -> get the id of the option
+# id = 1: Thúc đẩy kinh doanh
+# id = 4: Nâng cao hiệu suất làm việc
+# id = 5: Tư vấn tổng thể về giải pháp B-ERP
+
     if type_db == 'CRM-BASE':
         subsystem = 1
     elif type_db == 'PM-BASE':
-        subsystem = 2
+        subsystem = 4
     lead_data = {
         'name': 'Tạo database thành công',
         'contact_name': config['DATABASE']['name_user'],
